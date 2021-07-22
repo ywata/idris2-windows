980ae87c69b194f50ef487e4679d26a7ce72c49c
Yasu Watanabe <ywata@protonmail.com>
2021-07-22 10:59:32 +0900

    Update location of idris related files.

--- configure.ps1
+++ configure.ps1
@@ -4,29 +4,35 @@
 
 $configBuilder = [System.Text.StringBuilder]""
 
+$idrisVersion = "0.4.0"
+
 $rootPath = $pwd.Path
 # PATH
-$idrisPath = $rootPath + "\idris2\.idris2"
+$idrisPath = $rootPath + "\.idris2"
 # IDRIS2_DATA
-$idrisData = $rootPath + "\idris2\bootstrap\idris2-0.3.0\support"
+$idrisData = $idrisPath + "\idris2-" + $idrisVersion + "\support"
 # IDRIS2_LIBS
-$idrisLibs = $rootPath + "\idris2\bootstrap\idris2-0.3.0\lib"
+$idrisLibs = $idrisPath + "\idris2-" + $idrisVersion + "\lib"
 # IDRIS2_PREFIX
-$idrisPrefix = $rootPath + "\idris2\bootstrap"
+$idrisPrefix = $idrisPath
 # IDRIS2_PATH
 $idrisAppPath = $idrisPath + "\bin\idris2_app"
+
+# Add these to PATH
 $schemePath = $rootPath + "\chez\bin\ta6nt"
+$idrisBinPath = $idrisPath + "\bin"
 
 [void]$configBuilder.AppendLine("@echo off")
 [void]$configBuilder.AppendLine("set PATH=$idrisAppPath;%PATH%")
 [void]$configBuilder.AppendLine("set PATH=$schemePath;%PATH%")
-[void]$configBuilder.AppendLine("set IDRIS2_PATH=$idrisPath")
-[void]$configBuilder.AppendLine("set IDRIS2_PREFIX=$idrisPrefix")
-[void]$configBuilder.AppendLine("set IDRIS2_LIBS=$idrisLibs")
-[void]$configBuilder.AppendLine("set IDRIS2_DATA=$idrisData")
-[void]$configBuilder.AppendLine("`"$schemePath/scheme.exe`" --script `"$($idrisAppPath + '\idris2.so')`" %*")
 
-$outFile = $idrisPath + "\bin\idris2.cmd"
+[void]$configBuilder.AppendLine("rem set IDRIS2_PATH=$idrisPath")
+[void]$configBuilder.AppendLine("set IDRIS2_PREFIX=$idrisPrefix")
+[void]$configBuilder.AppendLine("rem set IDRIS2_LIBS=$idrisLibs")
+[void]$configBuilder.AppendLine("rem set IDRIS2_DATA=$idrisData")
+[void]$configBuilder.AppendLine("`"$schemePath/scheme.exe`" --program `"$($idrisAppPath + '\idris2.so')`" %*")
+
+$outFile = $idrisBinPath + "\idris2.cmd"
 
 Set-Content -Path $outFile -Value $configBuilder.ToString()
 Write-Output "The configuration was created in:"
