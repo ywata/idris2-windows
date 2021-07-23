731321acbf588c92f8daf7ee352aae755eff8119
Yasu Watanabe <ywata@protonmail.com>
2021-07-23 21:28:50 +0900

    configure.ps1 generates two level powershell script
    Originally, the script generates .cmd but it does not work with VS-Code. It now generates .cmd which calls powershell to run .ps1 script. Original .cmd is also generated -.cmd.

--- configure.ps1
+++ configure.ps1
@@ -6,18 +6,56 @@
 $idrisVersion = "0.4.0"
 
 # The script assumes sha256sum exists in $hashBin
-$hashBin = "C:\msys64\usr\bin"
+$hashBinPath = "C:\msys64\usr\bin"
 
 $scriptDirPath = $PSScriptRoot
 $archiveRoot = (get-item $scriptDirPath).parent.parent.FullName
 $idrisPrefix = $archiveRoot + "\.idris2"
 $idrisBinPath = $idrisPrefix + "\bin"
+$idrisLibPath = $idrisPrefix + "\lib"
 # $idrisData is not used at thismoment
 #$idrisData = $idrisPrefix + "/idris2-" + $idrisVersion
 $schemePath  = $archiveRoot + "\chez\bin\ta6nt"
 
 # Create script contents. I with we have here document.
-function Idris2-Cmd-String {
+function Idris2-Cmd-String{
+    param(
+	$appName
+    )
+    $idrisAppPath = $idrisBinPath + "\" + $appName + "_app\"
+    $CMD = @"
+set PWSHCMD=`"$idrisBinPath\$appName.ps1`"
+powershell -ExecutionPolicy Bypass -File `%PWSHCMD`%
+"@
+    Write-Output $CMD
+}
+
+function Idris2-PS1-String {
+    param(
+	$appName
+    )
+    $idrisAppPath = "$idrisBinPath\$appName" + "_app"
+    $CMD = @"
+`$env:Path += `";$schemePath`"
+`$env:Path += `";$hashBinPath`"
+`$env:Path += `";$idrisAppPath`"
+
+`$env:IDRIS2_PREFIX = `"$idrisPrefix`"
+`$env:IDRIS2_LIB = `"$idrisLibPath`"
+`&`"$schemePath/scheme.exe`" --program `"$idrisAppPath/$appName.so`" `$args
+"@
+    $configBuilder = [System.Text.StringBuilder]""
+    [void]$configBuilder.AppendLine("@echo off")
+    [void]$configBuilder.AppendLine("set PATH=$schemePath;%PATH%;" + $hashBin)
+    [void]$configBuilder.AppendLine("set PATH=$idrisAppPath;%PATH%")
+    [void]$configBuilder.AppendLine("set PATH=%PATH%;" + "C:\msys64\usr\bin")
+    [void]$configBuilder.AppendLine("set IDRIS2_PREFIX=$idrisPrefix")
+#    [void]$configBuilder.AppendLine("set IDRIS2_LIB=$idrisPrefix" + "\lib")
+    [void]$configBuilder.AppendLine("`"$schemePath/scheme.exe`" --script $idrisAppPath" + "/" + $appName + ".so" +  " %*")
+    Write-Output $CMD
+}
+
+function Idris2-OldCmd-String {
     param(
 	$appName
     )
@@ -30,21 +68,22 @@ function Idris2-Cmd-String {
     [void]$configBuilder.AppendLine("set PATH=%PATH%;" + "C:\msys64\usr\bin")
     [void]$configBuilder.AppendLine("set IDRIS2_PREFIX=$idrisPrefix")
 #    [void]$configBuilder.AppendLine("set IDRIS2_LIB=$idrisPrefix" + "\lib")
-    [void]$configBuilder.AppendLine("`"$schemePath/scheme.exe`" --script $idrisAppPath" + "/" + $appName + ".so" +  " %*")
+    [void]$configBuilder.AppendLine("`"$schemePath/scheme.exe`" --program $idrisAppPath" + "/" + $appName + ".so" +  " %*")
     Write-Output $configBuilder.ToString()
 }
 
-function Create-Idris2-Cmd {
+function Create-Idris2-Script {
     param(
 	$Path,
 	$Contents
     )
     Set-Content -Path $Path -Value $Contents
-    Write-Output "The configuration was created in:"
-    Write-Output $path
+    Write-Host "The configuration was created in:$Path"
+    Write-Host "$Contents"
+    Write-Output $Path
 }
 
-Write-Host "We expect sha256sum or its replacement is in " $hashBin
+Write-Host "We expect sha256sum or its replacement is in $hashBin"
 
 # We consider _app is created by idris2 to store scheme object file.
 $apps = Get-ChildItem -Path *_app | % {$_.ToString()} | % {Split-Path -Path $_ -Leaf} | %{$_.SubString(0, $_.Length -4)}
@@ -52,6 +91,10 @@ $apps = Get-ChildItem -Path *_app | % {$_.ToString()} | % {Split-Path -Path $_ -
 # apps contains base name without _app.
 $apps | %{
   $cmd = Idris2-Cmd-String $_
-  Create-Idris2-Cmd -Path ($idrisBinPath + "\" + $_ + ".cmd") -Contents $cmd
+  Create-Idris2-Script -Path "$idrisBinPath\$_.cmd" -Contents $cmd
+  $ps1 = Idris2-PS1-String $_
+  Create-Idris2-Script -Path "$idrisBinPath\$_.ps1" -Contents $ps1
+  $oldCmd = Idris2-OldCmd-String $_
+  Create-Idris2-Script -Path "$idrisBinPath\$_-.cmd" -Contents $oldCmd
 }
 
